ec9de83e51da0ae3f371d7c1cecce52e2875464a
ShineWaiYanAung <chantharpro89@gmail.com>
2024-06-23 21:38:15 +0630

    BlocCreation

---
+++ lib/Bloc/cart_event.dart
@@ -0,0 +1,19 @@
+part of 'cart_bloc.dart';
+
+class CartEvent extends Equatable{
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class CartIncrement extends CartEvent{
+  FishName  items;
+  CartIncrement(this.items);
+  List<Object?> get props => [items];
+}
+class CartDecrement extends CartEvent{
+  FishName items;
+  CartDecrement(this.items);
+  List<Object?> get props => [items];
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kg6_project/Bloc/cart_bloc.dart';
 import 'package:kg6_project/Screen/FishCategory.dart';
 
 import 'Screen/homeScreen.dart';
@@ -13,9 +15,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Money Recepit App',
-      home: HomeScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CartBloc(),
+        ),
+      ],
+      child: const MaterialApp(
+        title: 'Money Recepit App',
+        home: HomeScreen(),
+      ),
     );
   }
 }
